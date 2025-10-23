19a81541bbd04719251ef041db7678ef7faa1281
mario.cuberos@grupokonecta.com <mario.cuberos@grupokonecta.com>
2025-10-23 15:20:39 -0500

    ✨ feat(main): add global validation pipe with whitelist option

--- backend/manager/apps/manager-api-gateway/src/main.ts
+++ backend/manager/apps/manager-api-gateway/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ManagerApiGatewayModule } from './manager-api-gateway.module';
 import { envs } from './config/envs';
-import { LoggerService } from '@nestjs/common';
+import { LoggerService, ValidationPipe } from '@nestjs/common';
 import { LOGGER_SERVICE_SYMBOL } from '@app/logger/constants/logger-symbol.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(ManagerApiGatewayModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
 
   const logger = app.get<LoggerService>(LOGGER_SERVICE_SYMBOL);
   app.useLogger(logger);
