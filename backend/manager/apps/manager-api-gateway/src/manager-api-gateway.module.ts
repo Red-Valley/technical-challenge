4718b057f09531590116a5a5520448da659a2f33
mario.cuberos@grupokonecta.com <mario.cuberos@grupokonecta.com>
2025-10-23 21:17:21 -0500

    ✨ feat(manager-api-gateway): add StatusHistoriesModule to imports

--- backend/manager/apps/manager-api-gateway/src/manager-api-gateway.module.ts
+++ backend/manager/apps/manager-api-gateway/src/manager-api-gateway.module.ts
@@ -3,9 +3,10 @@ import { PatientsModule } from './patients/patients.module';
 import { LoggerModule } from '@app/logger/logger.module';
 import { ProvidersModule } from './providers/providers.module';
 import { StatusesModule } from './statuses/statuses.module';
+import { StatusHistoriesModule } from './status-histories/status-histories.module';
 
 @Module({
-  imports: [PatientsModule, LoggerModule, ProvidersModule, StatusesModule],
+  imports: [PatientsModule, LoggerModule, ProvidersModule, StatusesModule, StatusHistoriesModule],
   controllers: [],
   providers: [],
 })
