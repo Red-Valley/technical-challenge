8e498a96cb2afed941c6621c64c0dcfa858fd562
mario.cuberos@grupokonecta.com <mario.cuberos@grupokonecta.com>
2025-10-24 15:11:21 -0500

    ✨ feat(constants): add query keys for providers, patients, and statuses

---
+++ frontend/manager-app/src/utils/constants/query-key-constants/queryKeyConstants.js
@@ -0,0 +1,8 @@
+export const QUERY_KEYS = {
+  GET_ALL_PROVIDERS: "get-all-providers",
+  GET_ALL_PATIENTS: "get-all-patients",
+  CREATE_PROVIDER: "create-provider",
+  CREATE_PATIENT: "create-patient",
+  GET_ALL_STATUSES: "get-all-statuses",
+  CREATE_STATUS: "create-status"
+}
