098d163ac534fc8d6f45993d81fdd3d0b475f532
mario.cuberos@grupokonecta.com <mario.cuberos@grupokonecta.com>
2025-10-24 15:10:55 -0500

    ✨ feat(provider): refactor useProvider hook for clarity

--- frontend/manager-app/src/hooks/dashboard/provider/useProvider.jsx
+++ frontend/manager-app/src/hooks/dashboard/provider/useProvider.jsx
@@ -1,51 +1,46 @@
-import { useEffect } from "react";
 import { useFormik } from "formik";
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
 import { initialValues } from "./constants/form-constants/initialValues";
 import { validationSchema } from "./constants/form-constants/validationSchema";
 import { useProviderStore } from "../../../stores/provider/providerStore";
 import { setInitialValues } from "../../../utils/form/setInitialValues";
 import { createProvider } from "../../../services/provider/providerService";
 import { infoAlert } from "../../../services/alert/alertService";
-import { QUERY_KEYS } from "../../../utils/constants/query-key-constants/queryKeyConstants";
 
 export const useProvider = () => {
-
-  const providerStore = useProviderStore(state => state);
+  const providerStore = useProviderStore((state) => state);
 
   const onSubmit = () => {
-    refetch();
-  }
+    createProviderMutation.mutate(form.values);
+  };
 
   const form = useFormik({
     initialValues: setInitialValues(providerStore, initialValues),
     validationSchema,
-    onSubmit
-  })
+    onSubmit,
+  });
 
-  const { data, isLoading, refetch, isSuccess } = useQuery({
-    queryKey: [QUERY_KEYS.CREATE_PROVIDER, form.values],
-    queryFn: createProvider,
-    enabled: false
-  })
-
-  useEffect(() => {
-    if(!isSuccess) return;
-
+  const handleOnSaveSuccess = () => {
     infoAlert({
-      icon: 'success',
-      title: 'Proveedor creado',
-      message: 'El proveedor ha sido creado exitosamente.'
+      icon: "success",
+      title: "Provider created",
+      message: "The provider has been created successfully.",
     });
-
     form.resetForm();
-  }, [isSuccess])
-  
+  }
+
+  const createProviderMutation = useMutation({
+    mutationFn: createProvider,
+    onError: () => {
+      errorAlert({ message: "Error creating this provider" });
+    },
+    onSuccess: handleOnSaveSuccess
+  });
 
   return {
     form,
     providerStore,
-    isLoading,
-    onSubmit
-  }
-}
+    loading: createProviderMutation.isLoading,
+    onSubmit,
+  };
+};
